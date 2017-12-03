8aa1e8cd2280f893d5178847d761770d7b654ad1
Luiz Eduardo <luizeduardokowalski@gmail.com>
2017-12-03 01:15:59 +0100

    Tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :packages, only: %i[index]
+  root 'packages#index'
 end
